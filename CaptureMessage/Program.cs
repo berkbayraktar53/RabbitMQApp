9cda336255db5b677e1d8932686e0ad11b4e83f6
Kerem Berk Bayraktar <keremberk53@gmail.com>
2023-02-25 15:01:13 +0300

    Added rabbitmq package

---
+++ CaptureMessage/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace CaptureMessage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory
+            {
+                Uri = new Uri("amqps://hxlbuuxg:2PKW_2gCfkHNT8-IxAVDWfVpInGthCte@rattlesnake.rmq.cloudamqp.com/hxlbuuxg")
+            };
+            using var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+            channel.QueueDeclare("mesaj-kuyruk", true, false, false);
+            var consumer = new EventingBasicConsumer(channel);
+            channel.BasicConsume("mesaj-kuyruk", true, consumer);
+            consumer.Received += Consumer_Received;
+            Console.ReadLine();
+        }
+        private static void Consumer_Received(object sender, BasicDeliverEventArgs e)
+        {
+            Console.WriteLine("Gelen Mesaj: " + Encoding.UTF8.GetString(e.Body.ToArray()));
+        }
+    }
+}
---
+++ SendMessage/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace SendMessage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                SendMessage();
+            }
+        }
+
+        public static void SendMessage()
+        {
+            var factory = new ConnectionFactory
+            {
+                Uri = new Uri("amqps://hxlbuuxg:2PKW_2gCfkHNT8-IxAVDWfVpInGthCte@rattlesnake.rmq.cloudamqp.com/hxlbuuxg")
+            };
+            using var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+            channel.QueueDeclare("mesaj-kuyruk", true, false, false);
+            var mesaj = "Bu bir test mesajıdır";
+            var body = Encoding.UTF8.GetBytes(mesaj);
+            channel.BasicPublish(String.Empty, "mesaj-kuyruk", null, body);
+            Console.WriteLine(mesaj);
+            Console.ReadLine();
+        }
+    }
+}
